467ef31ec9c50e3b57c23392e49556eeab667512
Edward Kang <12953840@student.uts.edu.au>
2021-06-09 00:12:57 +1000

    final revision

--- english_dictionary/english_dictionary.py
+++ english_dictionary/english_dictionary.py
@@ -4,7 +4,7 @@
 # If user mistypes a word that is similar, ask the user if it meant
 # X word. 
 # If user types jumbles, return word is not an word TO_DO
-# 1st Revision. Does not do mistyping finding
+
 import json
 import difflib
 from difflib import get_close_matches
@@ -14,14 +14,20 @@ def definition(word):
     word = word.lower()
     if word in data:
         return ' '.join(data.get(word))
-    elif not word in data:
+    elif get_close_matches(word, data.keys()):
         most_similar = get_close_matches(word, data.keys())[0]
-        inputa = input("Did you mean this word?: " + most_similar + " \nType 'Y' or 'N': ")
-        if inputa.lower() == 'y':
-            word = most_similar
-            return definition(word)
-        else:
-            return 'This word doesn''t exist'
+        while True:
+            inputa = input("Did you mean this word?: " + most_similar + 
+                        " \nType 'Y' or 'N': ")
+            if inputa.lower() == 'y':
+                word = most_similar
+                return definition(word)
+            elif inputa.lower() == 'n':
+                return 'This word doesn''t exist'
+            else:
+                print('Please say Y or N. \n')
+    else:
+        return 'This word doesn''t exist'
 
 while True:
     user_input = input("Say word: ")
