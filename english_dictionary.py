76a5cfe49bb1fd58cf4da7b18af3371af1f1baef
Edward Kang <12953840@student.uts.edu.au>
2021-06-08 16:31:03 +1000

    English Dictionary

---
+++ english_dictionary.py
@@ -0,0 +1,6 @@
+# Specifications
+# User Input: User inputs a word, Returns definition
+# Returns multiple definition if the word has multiple
+# If user mistypes a word that is similar, ask the user if it meant
+# X word. 
+# If user types jumbles, return word is not an word
